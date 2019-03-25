422be591c93e9609442e50c7734f2fe1a4c6cb66
ryanbarker1 <ryanbarker2@mail.weber.edu>
2019-03-25 11:00:41 -0600

    Ryan

--- Frontend/capstone/src/app/app-routing.module.ts
+++ Frontend/capstone/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EmployerComponent } from './employer/employer.component';
+import { JobListingComponent } from './job-listing/job-listing.component';
+import { QuestionForumComponent } from './question-forum/question-forum.component';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+{ path: '', redirectTo: '/home', pathMatch: 'full' },
+{path: '/home/employer', component: EmployerComponent},
+{path: '/home/job_listing', component: JobListingComponent},
+{path: '/home/question_forum', component: QuestionForumComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
