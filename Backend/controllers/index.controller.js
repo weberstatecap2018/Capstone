b12755419f66739be90ecafcb621c2bb81200dda
CChristiansen23 <christiansenconner@gmail.com>
2019-04-01 10:32:36 -0600

    same error still happening.

--- Backend/controllers/index.controller.js
+++ Backend/controllers/index.controller.js
@@ -3,7 +3,7 @@ let router = express.Router();
 
 router.get('/', function (req, res, next) {
   res.render('layout', { title: 'Capstone Project', 
-  content: "../../Frontend/capstone/src/app/qustion-forum/question-forum.component.html"});
+  content: "../../Frontend/capstone/src/app/qustion-forum/question-forum.component.html", errors: {}});
 });
 
 
